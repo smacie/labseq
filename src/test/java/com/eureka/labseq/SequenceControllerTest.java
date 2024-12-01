faf8105b22a777bf4f3283fc708fbcf34a36f6f4
salomao.junior <salomaomaciejunior1@gmail.com>
2024-12-01 09:34:27 +0000

    implemented few unit test

--- src/test/java/com/eureka/labseq/SequenceControllerTest.java
+++ src/test/java/com/eureka/labseq/SequenceControllerTest.java
@@ -2,6 +2,7 @@ package com.eureka.labseq;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
@@ -9,12 +10,55 @@ import static org.hamcrest.CoreMatchers.is;
 @QuarkusTest
 class SequenceControllerTest {
     @Test
-    void testHelloEndpoint() {
-        /*given()
+    void testUpService() {
+        given()
           .when().get("http://localhost:8080/labseq/v1/1")
           .then()
-             .statusCode(is(200));
-             //.body(is("Hello Spring"));*/
+             .statusCode(is(HttpStatus.OK.value()));
+    }
+
+    @Test
+    void testNegativeIndexHttpStatus() {
+        given()
+                .when().get("http://localhost:8080/labseq/v1/-1")
+                .then()
+                .statusCode(is(HttpStatus.BAD_REQUEST.value()));
+    }
+
+    @Test
+    void testSequenceValue0() {
+        given()
+                .when().get("http://localhost:8080/labseq/v1/0")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body(is("0"));
+    }
+
+    @Test
+    void testSequenceValue1() {
+        given()
+                .when().get("http://localhost:8080/labseq/v1/1")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body(is("1"));
+    }
+
+    @Test
+    void testSequenceValue2() {
+        given()
+                .when().get("http://localhost:8080/labseq/v1/2")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body(is("0"));
+    }
+
+    @Test
+    void testSequenceValue3() {
+        given()
+                .when().get("http://localhost:8080/labseq/v1/3")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body(is("1"));
     }
 
 }
---
+++ src/test/java/com/eureka/labseq/service/SequenceServiceTest.java
@@ -0,0 +1,12 @@
+package com.eureka.labseq.service;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+class SequenceServiceTest {
+    @Test
+    void testHelloEndpoint() {
+    }
+
+}
